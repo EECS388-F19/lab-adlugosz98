4728b32de201dc930c0269fe2b2fef0f8c3282e6
annadlug <annadlug@umich.edu>
2019-09-06 16:13:33 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+print("Tony Dlugosz")
+x = random.randint(0,101)
+y = random.randint(0,101)
+print(str(x))
+print(str(y))
+sum = x + y
+average = float(sum) / 2
+print("Sum= " + str(sum))
+print("Average= " + str(average))
+
